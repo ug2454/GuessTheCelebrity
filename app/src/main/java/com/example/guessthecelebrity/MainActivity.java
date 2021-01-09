9f1b0fa11cffc53bad73055f048de211b54f6dea
Uday Garg <u.garg14@gmail.com>
2021-01-09 16:15:02 +0530

    implemented some additional code to keep a score and show toast at the top

--- app/src/main/java/com/example/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/example/guessthecelebrity/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.guessthecelebrity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -37,17 +39,41 @@ public class MainActivity extends AppCompatActivity {
     int locationOfCorrectAnswer;
     ArrayList<Integer> result = new ArrayList<>();
     int randomImageNumber;
+    boolean isDataLoading = false;
+    int correctAnswer = 0;
+    int totalAnswer = 0;
+    TextView textView;
+    ArrayList<Integer> celebrityAlreadyFound = new ArrayList<>();
+
+    public  void resetScore(View view){
+        correctAnswer=0;
+        totalAnswer=0;
+        result.clear();
+        updateUI();
+
+    }
 
     public void updateUI() {
+
+        textView = findViewById(R.id.textView);
+        textView.setText(correctAnswer + " / " + totalAnswer);
+
         button1 = findViewById(R.id.button);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         button4 = findViewById(R.id.button4);
         randomImageNumber = random.nextInt(100) + 1;
+
+        while (celebrityAlreadyFound.contains(randomImageNumber)) {
+            randomImageNumber = random.nextInt(100) + 1;
+        }
+
         locationOfCorrectAnswer = random.nextInt(4);
         for (int i = 0; i < 4; i++) {
             if (i == locationOfCorrectAnswer) {
                 result.add(randomImageNumber);
+                celebrityAlreadyFound.add(randomImageNumber);
+
             } else {
                 int wrongAnswer = random.nextInt(100) + 1;
                 if (randomImageNumber == wrongAnswer) {
@@ -59,25 +85,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Picasso.with(this).load(celebrityURL.get(randomImageNumber)).into(imageView);
-        button1.setText(celebrityName.get(result.get(0)));
-        button2.setText(celebrityName.get(result.get(1)));
-        button3.setText(celebrityName.get(result.get(2)));
-        button4.setText(celebrityName.get(result.get(3)));
+        try {
+            button1.setText(celebrityName.get(result.get(0)));
+            button2.setText(celebrityName.get(result.get(1)));
+            button3.setText(celebrityName.get(result.get(2)));
+            button4.setText(celebrityName.get(result.get(3)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     public void selectCelebrity(View view) {
 
         button = (Button) view;
+        try {
+            if (button.getText().equals(celebrityName.get(randomImageNumber))) {
+                Toast toast = Toast.makeText(this, "You got it right!", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+                correctAnswer++;
+            } else {
 
-        if (button.getText().equals(celebrityName.get(randomImageNumber))) {
-            Toast.makeText(this, "You got it right!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Correct answer is " + celebrityName.get(randomImageNumber), Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(this, "Correct answer is " + celebrityName.get(randomImageNumber), Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+                toast.show();
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        totalAnswer++;
         result.clear();
-        updateUI();
-
+        try {
+            updateUI();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -85,12 +129,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         imageView = findViewById(R.id.imageView);
+
+
         Pattern pattern;
         Matcher matcher;
 
+
         DownloadWebContent task = new DownloadWebContent();
         String result = "";
+
         try {
             result = task.execute("https://www.imdb.com/list/ls052283250/").get();
             Log.i("result", result);
@@ -101,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
 
         pattern = Pattern.compile("src=\"(.*?)\"");
         matcher = pattern.matcher(result);
@@ -116,16 +166,12 @@ public class MainActivity extends AppCompatActivity {
             celebrityName.add(matcher.group(1));
         }
 
-        System.out.println(celebrityName.size());
 
-        System.out.println(celebrityURL.size());
         celebrityURL.remove(0);
         celebrityURL.remove(1);
         celebrityURL.remove(2);
         celebrityURL.remove(3);
         celebrityURL.remove(4);
-        System.out.println(celebrityURL.size());
-
 
 
         updateUI();
@@ -133,14 +179,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    static class DownloadWebContent extends AsyncTask<String, Void, String> {
+    class DownloadWebContent extends AsyncTask<String, Void, String> {
+
+
+
+
 
         @Override
         protected String doInBackground(String... urls) {
 
+
             StringBuilder result = new StringBuilder();
             URL url = null;
             try {
+
                 url = new URL(urls[0]);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 InputStream inputStream = connection.getInputStream();
@@ -162,5 +214,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+
+
     }
 }
